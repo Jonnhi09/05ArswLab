131f97843e82fbdb7af3ef08ded51186a0a65889
unknown <2103258@labinfo.is.escuelaing.edu.co>
2018-10-04 18:58:49 -0500

    Avance

--- src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintsPersistence.java
+++ src/main/java/edu/eci/arsw/blueprints/persistence/BlueprintsPersistence.java
@@ -30,4 +30,6 @@ public interface BlueprintsPersistence {
      */
     public Blueprint getBlueprint(String author,String bprintname) throws BlueprintNotFoundException;
     
+    
+    
 }
---
+++ src/main/java/edu/eci/arsw/blueprints/persistence/impl/AnotherBlueprintsPersistence.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.blueprints.persistence.impl;
+
+import edu.eci.arsw.blueprints.model.Blueprint;
+import edu.eci.arsw.blueprints.persistence.BlueprintNotFoundException;
+import edu.eci.arsw.blueprints.persistence.BlueprintPersistenceException;
+import edu.eci.arsw.blueprints.persistence.BlueprintsPersistence;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Jonathan Prieto
+ */
+@Service("Another")
+public class AnotherBlueprintsPersistence implements BlueprintsPersistence {
+
+    @Override
+    public void saveBlueprint(Blueprint bp) throws BlueprintPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Blueprint getBlueprint(String author, String bprintname) throws BlueprintNotFoundException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+}
